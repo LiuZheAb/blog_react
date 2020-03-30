190c553aa8e52c197594eaba820d979729cfa9ec
liuzhe <liu748951@163.com>
2020-03-30 08:46:06 +0800

    删除build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9dbf33b50685a3f88b58972fbe9eaef7.js"
+  "./precache-manifest.9669a0d0db5ccdc19aea10b69add5771.js"
 );
 
 self.addEventListener('message', (event) => {
