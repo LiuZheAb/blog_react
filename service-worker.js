d7f63abeaba10e5e4ad0cfc1f62207b51808511c
liuzhe <liu748951@163.com>
2020-04-11 05:09:51 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.46972190756c029748b6a8749a6f33b8.js"
+  "./precache-manifest.dcfc150b5810577eaf362fa924d2ecc5.js"
 );
 
 self.addEventListener('message', (event) => {
