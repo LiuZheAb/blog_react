4a64fa1fefa0ff1b8310dd3104fbf35b202d7d2c
liuzhe <liu748951@163.com>
2020-04-12 20:01:15 +0800

    内容组件

--- src/components/HomeContent/index.js
+++ src/components/HomeContent/index.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react'
+//内容组件
+import React, { Component, Suspense, lazy } from 'react';
 import { Layout } from 'antd';
 import { HashRouter as Router, Route } from "react-router-dom";
-import loadable from '../../utils/lazyLoad';
+import Loder from '../Loder';
+import HomeList from "../HomeList";
 import "./index.less";
 
 const { Content } = Layout;
-const GoJSintro = loadable(() => import('../GOJS/Intro'));
-const GoJSfirst = loadable(() => import('../GOJS/First'));
+const GoJS = lazy(() => import('../GOJS'));
+const MyBlog = lazy(() => import('../MyBlog'));
+const CodeReview = lazy(() => import('../CodeReview'));
+const PageFooter = lazy(() => import('../PageFooter'));
 
 export default class index extends Component {
+    componentDidMount() {
+        document.title = "Liuzhe's Blog";
+    }
     render() {
         return (
-            <Layout style={{ padding: '0 24px 24px' }}>
+            <Layout className="content-layout">
                 <Content
-                    className="site-layout-background"
                     style={{
                         padding: 24,
                         margin: 0,
                         minHeight: 280,
+                        background: "#fff"
                     }}
                 >
                     <Router>
-                        <Route path="/gojs-intro" render={() => (<GoJSintro />)} />
-                        <Route path="/gojs-1" render={() => (<GoJSfirst />)} />
+                        <Suspense fallback={<Loder />}>
+                            <Route exact path="/" render={() => { document.title = "Liuzhe's Blog"; return <HomeList /> }} />
+                            <Route path="/gojs" render={() => { document.title = "GoJS 入门教程"; return <GoJS /> }} />
+                            <Route path="/myblog" render={() => { document.title = "本站介绍"; return <MyBlog /> }} />
+                            <Route path="/codereview" render={() => { document.title = "Code Review"; return <CodeReview /> }} />
+                            <Route path={["/myblog", "/gojs", "/codereview"]} render={() => (<PageFooter />)} />
+                        </Suspense>
                     </Router>
                 </Content>
             </Layout>
