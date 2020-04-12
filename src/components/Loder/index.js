437abb7665cfbb981bc1d08bf76b647bf298570d
liuzhe <liu748951@163.com>
2020-04-12 20:01:39 +0800

    加载组件

---
+++ src/components/Loder/index.js
@@ -0,0 +1,15 @@
+//加载样式
+import React from 'react';
+import "./index.less";
+
+let index = () => {
+    return (
+        <div className="loader">
+            <div className="loader-content">
+                <div className="outer"></div>
+                <div className="inner"></div>
+            </div>
+        </div>
+    )
+}
+export default index;
