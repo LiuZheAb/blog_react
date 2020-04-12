b1263b1ce7154a3c82c1eaa6ea24670abc44c671
liuzhe <liu748951@163.com>
2020-04-12 20:01:01 +0800

    顶部Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,49 @@
+//头部Header组件
 import React, { Component } from 'react'
-import { Layout, 
-    // Menu 
-} from 'antd';
+import { Layout, } from 'antd';
+import {
+    Link, withRouter,
+    // Route 
+} from "react-router-dom";
+import Menu from "../Menu";
+import Breadcrumb from "../Breadcrumb";
+import IconFont from "../IconFont";
 import "./index.less";
 
 const { Header } = Layout;
 
-export default class index extends Component {
+class index extends Component {
+    state = {
+        display: "none"
+    }
+    // scrollTop() {
+    //     window.scrollTo({
+    //         left: 0,
+    //         top: 0,
+    //         behavior: 'smooth',
+    //     });
+    // }
     render() {
         return (
-            <Header className="header">
+            <Header className="header" id="header">
                 <div className="header-content">
-                    <div className="logo" />
-                    {/* <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['2']}
-                    >
-                        <Menu.Item key="1">nav 1</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
-                    </Menu> */}
+                    <div className="logo" title="首页">
+                        <Link to="/">
+                            <IconFont type="iconpc-dongman" />
+                        </Link>
+                    </div>
+                    <Breadcrumb />
+                        <Menu className="menu-horiz" mode="horizontal" />
+
+                    {/* <Route path="/gojs/:id" render={() => (
+                        <div className="back-to-top">
+                            <img src={require("../../assets/images/top.png")} alt="回到顶部" title="回到顶部" onClick={this.scrollTop} />
+                        </div>
+                    )} /> */}
                 </div>
             </Header>
         )
     }
 }
+
+export default withRouter(index);
