0875d0a0ad9f8db01c3354e8e176d242e9f71be9
liuzhe <liu748951@163.com>
2020-04-12 20:01:28 +0800

    IconFont地址

---
+++ src/components/IconFont/index.js
@@ -0,0 +1,8 @@
+//IconFont地址
+import { createFromIconfontCN } from '@ant-design/icons';
+
+const IconFont = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_1747626_d2h3aj24pn.js',
+});
+
+export default IconFont;
