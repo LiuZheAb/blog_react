261b6c25186ee6109db50cd3a820d75441b35719
liuzhe <liu748951@163.com>
2020-04-12 20:01:21 +0800

    首页文档列表

---
+++ src/components/HomeList/index.js
@@ -0,0 +1,69 @@
+//首页文档列表
+import React, { Component } from 'react';
+import { List } from 'antd';
+import { Link } from "react-router-dom";
+import { myblogData, gojsData, codeReview } from "../../assets/data";
+import IconFont from "../IconFont"
+import "./index.less";
+
+export default class index extends Component {
+    render() {
+        let dataArray = [myblogData, gojsData, codeReview];
+        let dataSource = [];
+        for (let i = 0; i < dataArray.length; i++) {
+            dataSource.push({
+                href: dataArray[i].baseHref,
+                className: dataArray[i].name,
+                title: dataArray[i].title,
+                description: dataArray[i].description,
+                img: dataArray[i].img,
+                content: dataArray[i].content,
+            })
+        }
+        return (
+            <div className="home-list">
+                <List
+                    itemLayout="vertical"
+                    size="large"
+                    dataSource={dataSource}
+                    renderItem={item => (
+                        <Link to={item.href}>
+                            <List.Item
+                                className="list-item"
+                                key={item.title}
+                                extra={
+                                    <div className="box">
+                                        <img src={require("../../assets/images/" + item.img)} alt={item.title} />
+                                        <div className="box-content">
+                                            <span className="title">点击进入</span>
+                                        </div>
+                                    </div >
+
+                                }
+                            >
+                                <List.Item.Meta
+                                    title={item.title}
+                                    description={item.description}
+                                />
+                                <p style={{ textIndent: "2em", color: "rgba(0, 0, 0, 0.65)" }}>{item.content}</p>
+                            </List.Item>
+                        </ Link>
+                    )}
+                />
+                <div className="footer">
+                    <div className="othersites">
+                        <span>我的站点：</span>
+                        <a target="_blank" rel="noopener noreferrer" href="https://music.163.com/#/playlist?id=117824494" title="网易云音乐"><IconFont type="iconwyy" /></a>
+                        <a target="_blank" style={{ fontSize: "34px", color: "#000" }} rel="noopener noreferrer" href="https://github.com/LiuZheAb/" title="Github"><IconFont type="icongithub" /></a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.yuque.com/liuzheab" title="语雀"><IconFont type="iconyuquemianlogo" /></a>
+                    </div>
+                    <div className="author">
+                        <div className="author-content">
+                            <span>&copy; 2020 Deved By <a target="_blank" rel="noopener noreferrer" href="https://github.com/LiuZheAb/" title="Github">LiuZheAb</a> </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
