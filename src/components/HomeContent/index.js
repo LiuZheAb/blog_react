afe2bd3d4d22c02d62a6379aea0122efcee4eaa6
liuzhe <liu748951@163.com>
2020-04-17 23:50:14 +0800

    UPDATE——将内容区组件改为数据map生成方式

--- src/components/HomeContent/index.js
+++ src/components/HomeContent/index.js
@@ -1,24 +1,32 @@
 //内容组件
 import React, { Component, Suspense, lazy } from 'react';
 import { Layout } from 'antd';
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, withRouter } from "react-router-dom";
 import Loder from '../Loder';
 import HomeList from "../HomeList";
+import { totalData } from "../../assets/data";
+import setKeyMap from "../../utils/keymap";
 import "./index.less";
 
 const { Content } = Layout;
-const GoJS = lazy(() => import('../Documents/GOJS'));
-const MyBlog = lazy(() => import('../Documents/MyBlog'));
-const CodeReview = lazy(() => import('../Documents/CodeReview'));
-const FreeCodeCamp = lazy(() => import('../Documents/FreeCodeCamp'));
-const Problem = lazy(() => import('../Documents/Problem'));
 const PageFooter = lazy(() => import('../PageFooter'));
+const Documents = lazy(() => import('../Documents'));
+let nameMap = setKeyMap(totalData);
+let hrefArray = [];
+for (let i = 0; i < totalData.length; i++) {
+    hrefArray.push(totalData[i].baseHref)
+}
 
-export default class index extends Component {
-    componentDidMount() {
-        document.title = "Liuzhe's Blog";
-    }
+class index extends Component {
     render() {
+        //根据路由确定当前的title
+        let path = this.props.location.pathname;
+        if (path !== "/") {
+            const pathSnippets = "/" + path.split('/').filter(i => i)[0];
+            document.title = pathSnippets === path ? nameMap[pathSnippets] : nameMap[pathSnippets] + "——" + nameMap[path];
+        } else {
+            document.title = "Liuzhe's Blog";
+        }
         return (
             <Layout className="content-layout">
                 <Content
@@ -30,13 +38,16 @@ export default class index extends Component {
                 >
                     <Router>
                         <Suspense fallback={<Loder />}>
-                            <Route exact path="/" render={() => { document.title = "Liuzhe's Blog"; return <HomeList /> }} />
-                            <Route path="/gojs" render={() => { document.title = "GoJS 入门教程"; return <GoJS /> }} />
-                            <Route path="/myblog" render={() => { document.title = "本站介绍"; return <MyBlog /> }} />
-                            <Route path="/codereview" render={() => { document.title = "Code Review"; return <CodeReview /> }} />
-                            <Route path="/freecodecamp" render={() => { document.title = "FreeCodeCamp"; return <FreeCodeCamp /> }} />
-                            <Route path="/problem" render={() => { document.title = "问题汇总"; return <Problem /> }} />
-                            <Route path={["/myblog", "/gojs", "/codereview", "/freecodecamp", "/problem"]} render={() => (<PageFooter />)} />
+                            <Route exact path="/" render={() => { return <HomeList /> }} />
+                            {hrefArray.map((item, index) => {
+                                return (
+                                    totalData[index].section.length > 0
+                                        ?
+                                        <Route key={index} path={item} component={() => <Documents dataSource={totalData[index]} />} />
+                                        : <Route key={index} path={item} component={lazy(() => import(`../Documents/${totalData[index].component}`))} />
+                                )
+                            })}
+                            <Route path={hrefArray} render={() => (<PageFooter />)} />
                         </Suspense>
                     </Router>
                 </Content>
@@ -44,3 +55,5 @@ export default class index extends Component {
         )
     }
 }
+
+export default withRouter(index);
