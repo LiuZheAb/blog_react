5854019cf26ad81781690e96332952841f27326e
liuzhe <liu748951@163.com>
2020-04-17 23:40:27 +0800

    UPDATE——修改data文件数据结构,添加component键；将catalog修正为section,将section修正为chapter；section数据简化,删掉href只保留title,并将对象数组改为字符串数组

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,92 +1,50 @@
 //网站数据统一配置
+//本站介绍
 export let myblogData = {
     name: "myblog",
     title: "本站介绍",
     baseHref: "/myblog",
+    component:"MyBlog",
     description: "建站经历",
     img: "myblog.png",
     content: "建站过程中的随笔。",
-    catalog: [],
-    section: {}
+    section: [],
+    chapter: {}
 };
-
+//GoJS 教程
 export let gojsData = {
     name: "gojs",
     title: "GoJS 教程",
     baseHref: "/gojs",
+    component:"GOJS",
     description: "GoJS 简介",
     img: "gojs.png",
     content: "GoJS是一个用于构建交互式图表和图形的JavaScript和TypeScript库。GoJS允许你建立各种各样的图表和图形，从简单的流程图和组织图到非常具体的工业图，SCADA和BPMN图，医学图，如基因图和疫情建模图等。GoJS使用可定制的模板和布局使构建复杂节点、链接和组的JavaScript关系图变得更容易。",
-    catalog: [
-        {
-            href: "/gojs/0",
-            title: "写在前面"
-        }, {
-            href: "/gojs/1",
-            title: "GoJS 介绍"
-        }, {
-            href: "/gojs/2",
-            title: "第一节 简单的GoJS demo"
-        }, {
-            href: "/gojs/3",
-            title: "第二节 GoJS应用中有什么"
-        }, {
-            href: "/gojs/4",
-            title: "第三节 使用GoJS构建Parts(Parts 和 Panels)"
-        }, {
-            href: "/gojs/5",
-            title: "第四节 GoJS Diatram Model"
-        }, {
-            href: "/gojs/6",
-            title: "第五节 使用GoJS构建Parts(Templates)"
-        }, {
-            href: "/gojs/7",
-            title: "第六节 GoJS GraphObject属性"
-        }, {
-            href: "/gojs/8",
-            title: "第一节 GoJS布局教程概述"
-        }, {
-            href: "/gojs/9",
-            title: "第二节 网格布局(GridLayout)"
-        }, {
-            href: "/gojs/10",
-            title: "第三节 树形布局(TreeLayout)"
-        }, {
-            href: "/gojs/11",
-            title: "第四节 力导向布局(ForceDirectedLayout)"
-        }, {
-            href: "/gojs/12",
-            title: "第五节 层次布局(LayeredDigraphLayout)"
-        }, {
-            href: "/gojs/13",
-            title: "第六节 环状布局(CircularLayout)"
-        }, {
-            href: "/gojs/14",
-            title: "第一节 表格面板(Table Panel)"
-        }, {
-            href: "/gojs/15",
-            title: "第二节 网格面板(Grid Panel)"
-        }, {
-            href: "/gojs/16",
-            title: "第三节 刻度面板(Graduated Panel)"
-        }, {
-            href: "/gojs/17",
-            title: "第一节 调整大小,旋转,装饰"
-        }, {
-            href: "/gojs/18",
-            title: "第二节 概述"
-        }, {
-            href: "/gojs/19",
-            title: "第三节 重新链接,链接重塑,零件"
-        }, {
-            href: "/gojs/20",
-            title: "GoJS 端口(Ports)"
-        }, {
-            href: "/gojs/21",
-            title: "GoJS Item Arrays"
-        }
+    section: [
+        "写在前面",
+        "GoJS 介绍",
+        "第一节 简单的GoJS demo",
+        "第二节 GoJS应用中有什么",
+        "第三节 使用GoJS构建Parts(Parts 和 Panels)",
+        "第四节 GoJS Diatram Model",
+        "第五节 使用GoJS构建Parts(Templates)",
+        "第六节 GoJS GraphObject属性",
+        "第一节 GoJS布局教程概述",
+        "第二节 网格布局(GridLayout)",
+        "第三节 树形布局(TreeLayout)",
+        "第四节 力导向布局(ForceDirectedLayout)",
+        "第五节 层次布局(LayeredDigraphLayout)",
+        "第六节 环状布局(CircularLayout)",
+        "第一节 表格面板(Table Panel)",
+        "第二节 网格面板(Grid Panel)",
+        "第三节 刻度面板(Graduated Panel)",
+        "第一节 调整大小,旋转,装饰",
+        "第二节 概述",
+        "第三节 重新链接,链接重塑,零件",
+        "GoJS 端口(Ports)",
+        "GoJS Item Arrays",
     ],
-    section: {
+    chapter: {
         "写在前面": [0, 0],
         "GoJS 介绍": [1, 1],
         "GoJS 入门教程": [2, 7],
@@ -97,97 +55,70 @@ export let gojsData = {
         "GoJS Item Arrays": [21, 21],
     }
 };
-
+//Code Review
 export let codeReview = {
     name: "codereview",
     title: "Code Review",
     baseHref: "/codereview",
+    component:"CodeReview",
     description: "Code Review(代码评审)简介",
     img: "codeReview.png",
     content: "该部分译自谷歌工程实践文档(Google Engineering Practices Documentation)。代码评审也称代码复查，是指通过阅读代码来检查源代码与编码标准的符合性以及代码质量的活动。",
-    catalog: [
-        {
-            href: "/codereview/0",
-            title: "简介"
-        }, {
-            href: "/codereview/1",
-            title: "代码评审标准"
-        }, {
-            href: "/codereview/2",
-            title: "代码评审要点"
-        }, {
-            href: "/codereview/3",
-            title: "在评审中如何查看CL"
-        }, {
-            href: "/codereview/4",
-            title: "代码评审的速度"
-        }, {
-            href: "/codereview/5",
-            title: "如何编写代码评审的评论"
-        }, {
-            href: "/codereview/6",
-            title: "如何处理评审后程序员拒绝修改的情况"
-        }, {
-            href: "/codereview/7",
-            title: "写出好的CL"
-        }, {
-            href: "/codereview/8",
-            title: "小型CL"
-        }, {
-            href: "/codereview/9",
-            title: "如何处理评审人员的评论"
-        },
+    section: ["简介",
+        "代码评审标准",
+        "代码评审要点",
+        "在评审中如何查看CL",
+        "代码评审的速度",
+        "如何编写代码评审的评论",
+        "如何处理评审后程序员拒绝修改的情况",
+        "写出好的CL",
+        "小型CL",
+        "如何处理评审人员的评论",
     ],
-    section: {
+    chapter: {
         "简介": [0, 0],
         "代码评审者指南": [1, 6],
         "代码开发者指南": [7, 9],
     }
 };
-
+//FreeCodeCamp
 export let freeCodeCamp = {
     name: "freecodecamp",
     title: "FreeCodeCamp",
     baseHref: "/freecodecamp",
+    component:"FreeCodeCamp",
     description: "FreeCodeCamp学习笔记",
     img: "freecodecamp.png",
     content: "Github上stars最多的项目，免费编码学习社区。",
-    catalog: [
-        {
-            href: "/freecodecamp/0",
-            title: "简介"
-        }, {
-            href: "/freecodecamp/1",
-            title: "var()"
-        }, {
-            href: "/freecodecamp/2",
-            title: "linear-gradient()"
-        },
+    section: [
+        "简介",
+        "var()",
+        "linear-gradient()",
+        "scale()",
+        "scale()",
     ],
-    section: {
+    chapter: {
         "简介": [0, 0],
-        "css篇": [1, 2],
+        "css篇": [1, 3],
     }
 };
-
+//问题汇总
 export let proplemData = {
     name: "proplem",
     title: "问题汇总",
     baseHref: "/problem",
+    component:"Problem",
     description: "开发中遇到的问题",
     img: "problem.jpg",
     content: "用来记录开发中遇到的各种问题。",
-    catalog: [
-        {
-            href: "/problem/0",
-            title: "无法设置对象属性"
-        }, {
-            href: "/problem/1",
-            title: "问题2"
-        }
+    section: [
+        "react-router问题",
+        "无法设置对象属性",
+        "问题2",
     ],
-    section: {
-        "JS篇": [0, 1],
+    chapter: {
+        "React篇":[0,0],
+        "JS篇": [1, 2],
     }
 };
 export let totalData = [myblogData, gojsData, codeReview, freeCodeCamp, proplemData];
