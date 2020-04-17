0b0fc499616869b2f57e997a0a5be03893c63957
liuzhe <liu748951@163.com>
2020-04-17 23:47:20 +0800

    UPDATE——修改本站介绍文档内容

--- src/components/Documents/MyBlog/index.js
+++ src/components/Documents/MyBlog/index.js
@@ -25,7 +25,6 @@ export default class index extends Component {
         this.setState({ articleTree });
     }
     componentDidMount() {
-        document.title = "本站介绍";
         this.getArticleTree();
     }
     render() {
@@ -44,13 +43,13 @@ export default class index extends Component {
                         <p className="indent-2">第一时间想到的就是最笨的方法，纯HTML、CSS肯定是行得通的。而且语雀有自带的将文档转为HTML的功能，还是很好用的，所以就想着先在语雀里写好文档，再转成HTML，然后在需要的位置插入代码。随后把想法测试了一下，放到gp上没问题，开心。</p>
                         <p className="indent-2">既然都建站了，索性就弄个博客吧，上面的办法可以是可以，可是每个页面都要写个HTML文件，这未免也太蠢了，想想果断放弃了。</p>
                         <p className="indent-2">然后就自然而然地开始找博客模板，然后就找到了个可以快速构建个人博客的好东西，Hexo，想了解的小伙伴请移步<a href="https://hexo.io/" target="_blank" rel="noopener noreferrer">Hexo官网</a>。</p>
-                        <p className="indent-2">工具找好了，下一步是什么？使用？别急，先找教程，省时省力不掉坑。找了有几篇，最后参考的是这个<a href="https://zhuanlan.zhihu.com/p/88481760" target="_blank" rel="noopener noreferrer">https://zhuanlan.zhihu.com/p/88481760</a>，文章写得很详细，手把手教学，可是我上来就忽略了第一点，<b>仓库名应该为：用户名.github.io </b>，后边的泪就不提了。。。</p>
+                        <p className="indent-2">工具找好了，下一步是什么？使用？别急，先找教程，省时省力不掉坑。找了有几篇，最后参考的是这个<a href="https://zhuanlan.zhihu.com/p/26625249" target="_blank" rel="noopener noreferrer">https://zhuanlan.zhihu.com/p/26625249</a>，文章写得很详细，手把手教学，可是我上来就忽略了第一点，<b>仓库名应该为：用户名.github.io </b>，后边的泪就不提了。。。</p>
                         <p className="indent-2">Hexo除了快速建站之外，还有很多主题选择，感兴趣的可以看一下<a href="https://hexo.io/themes/" target="_blank" rel="noopener noreferrer">Hexo官方Themes</a>，将近300个，真的很多，而且拉取、配置都很便捷。</p>
                         <p className="indent-2">折腾了大概有三四个小时，其实大部分时间都用在挑主题上了。。。然后就正式开始搞，然后就没有然后了。。。开始用我才发现，Hexo用Markdown解析文章，它只能解析文章啊，还是不能满足需求随心所欲的插HTML元素（也可能没学会怎么用），关键是感觉写起来还挺麻烦的，所以又放弃Hexo了。</p>
                         <p className="indent-2">随后又开始找新的办法，终于想到了用自己比较熟悉的React来建站，老路子，教程参考这里<a href="https://zhuanlan.zhihu.com/p/88481760" target="_blank" rel="noopener noreferrer">https://zhuanlan.zhihu.com/p/88481760</a>。</p>
                         <p className="indent-2">测试了一下，将React打包后的代码丢到github上，可行，于是就这么愉快地决定了。</p>
                         <br />
-                        <p className="indent-2">以上基本上就是建站的整个心路历程。</p>
+                        <p className="indent-2">以上基本上就是建站的整个历程。</p>
                         <br />
                         <div className="text-right">
                             <em>写于2020-4-6凌晨</em>
