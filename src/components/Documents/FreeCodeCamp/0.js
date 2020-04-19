6dd9a196dc01f4d4bc7ee178e639aad52a013ce3
liuzhe <liu748951@163.com>
2020-04-19 16:41:07 +0800

    UPDATE——添加在FreeCodeCamp练习中遇到的例子

--- src/components/Documents/FreeCodeCamp/0.js
+++ src/components/Documents/FreeCodeCamp/0.js
@@ -13,6 +13,12 @@ export default class index extends Component {
                         <h2>写在前面</h2>
                         <p className="indent-2">FreeCodeCamp是Github上star最多的项目,免费编码学习社区。官方地址:<a href="https://www.freecodecamp.org" target="_blank" rel="noopener noreferrer">https://www.freecodecamp.org</a></p>
                         <p className="indent-2">本文档主要是记录学到的内容。</p>
+                        <h2>案例</h2>
+                        <ul>
+                            <li><a href="https://www.freecodecamp.org/learn/responsive-web-design/applied-visual-design/create-a-graphic-using-css" target="_blank" rel="noopener noreferrer">月牙形状</a></li>
+                            <li><a href="https://www.freecodecamp.org/learn/responsive-web-design/applied-visual-design/create-a-more-complex-shape-using-css-and-html" target="_blank" rel="noopener noreferrer">心形</a></li>
+                        
+                        </ul>
                     </div>
                 </div>
                 <Directory />
