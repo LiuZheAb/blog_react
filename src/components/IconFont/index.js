ac3d510ab7ee0664f561032cd416c5a743929aaa
liuzhe <liu748951@163.com>
2020-04-30 22:13:46 +0800

    UPDATE——logo样式

--- src/components/IconFont/index.js
+++ src/components/IconFont/index.js
@@ -2,7 +2,7 @@
 import { createFromIconfontCN } from '@ant-design/icons';
 
 const IconFont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_1747626_xsmr9b21nn.js',
+  scriptUrl: '//at.alicdn.com/t/font_1747626_zt1y19imrxq.js',
 });
 
 export default IconFont;
