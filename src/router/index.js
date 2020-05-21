e1a651c0da72d5adb5ff39dbe03e37a783b97ff4
liuzhe <liu748951@163.com>
2020-05-22 00:10:56 +0800

    NEW——自定义表单demo

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@
 import React, { Suspense, lazy } from 'react';
 import { Switch, HashRouter as Router, Route } from "react-router-dom";
 import Loder from '../components/Loder';
+
 const Home = lazy(() => import('../pages/Home'));
+const CustomForm = lazy(() => import('../pages/CustomForm'));
 
 export default class EarthRouter extends React.Component {
     render() {
@@ -10,6 +12,8 @@ export default class EarthRouter extends React.Component {
             <Router>
                 <Suspense fallback={<Loder />}>
                     <Switch>
+                        <Route path="/customform" render={() => <CustomForm />} />
+                        <Route path="/webide" render={() => <Home />} />
                         <Route path="/" render={() => <Home />} />
                     </Switch>
                 </Suspense>
